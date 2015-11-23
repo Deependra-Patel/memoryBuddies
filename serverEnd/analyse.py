ff7d4499dbd1d8173f3ea7fadb12de5908434331
Jayesh Bageriya <jayeshbageriya@nsl-53.cse.iitb.ac.in>
2015-11-24 02:19:15 +0530

    Added heuristic greedy algorithm for optimal VM placement. Also added an experiment case

--- serverEnd/analyse.py
+++ serverEnd/analyse.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python3
 
-f1 = open("../memTraces/Trace1/VM1.txt5", "rb")
-f2 = open("../memTraces/Trace1/VM1.txt6", "rb")
-a1 = []
-a2 = []
+def share_potential(str1, str2):
+	f1 = open(str1, "rb")
+	f2 = open(str2, "rb")
+	a1 = f1.readlines()
+	a2 = f2.readlines()
+	len1 = len(a1)
+	len2 = len(a2)
+	count = 0
+	print("No. of pages in ",str1,": " + str(len1))
+	print("No. of pages in ",str2,": " + str(len2))
 
-a1 = f1.readlines()
-a2 = f2.readlines()
-a1.sort()
-a2.sort()
-ind1 = 0
-ind2 = 0
-len1 = len(a1)
-len2 = len(a2)
-count = 0
-print("No. of pages in file 1: " + str(len1))
-print("No. of pages in file 2: " + str(len2))
+	all_pages = a1 + a2;
 
-while((ind1 < len1) and (ind2<len2)):
-    if (a1[ind1] == a2[ind2]):
-        count = count + 1
-        ind1 = ind1 + 1
-        ind2 = ind2 + 1
-        continue;
-    elif (a1[ind1] < a2[ind2]):
-        ind1 = ind1 + 1
-    else:
-        ind2 = ind2 + 1
+	count = len(set(all_pages));
 
-print("Common hashes: "+str(count))
+	print("Common hashes: "+str(count)," MB")
+	return count;
+
+def final_usage(li):
+	all_pages = []
+	for str in li:
+		f = open(str, "rb")
+		all_pages = all_pages + f.readlines()
+
+	return int(len(set(all_pages))/256);
+
+def intervm_sharing(str1, str2):
+	f1 = open(str1, "rb")
+	f2 = open(str2, "rb")
+	a1 = f1.readlines()
+	a2 = f2.readlines()
+	len1 = len(a1)
+	len2 = len(a2)
+	count = 0
+	print("No. of pages in ",str1,": " + str(len1))
+	print("No. of pages in ",str2,": " + str(len2))
+
+	all_pages = set(a1) & set(a2);
+
+	count = len(all_pages);
+	print("Common hashes: "+str(count/256)," MB")
+	return count;
---
+++ serverEnd/best_matching.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+
+from analyse import share_potential
+from analyse import final_usage
+from analyse import intervm_sharing
+
+file_name = ["../memTraces/Trace3/VM1.txt0",
+			 "../memTraces/kernelBuild/VM1.txt1",
+			 "../memTraces/Trace3/VM1.txt2",
+			 "../memTraces/Trace1/VM1.txt0",
+			 "../memTraces/Trace4/VM1.txt0"]
+
+vm_size = [1024, 2048, 1024, 2048, 1024]
+
+initial_map = [0,0,1,1,2]
+
+n = 5;
+share = [[0 for x in range(n)] for x in range(n)];
+for i in range(n):
+	for j in range(i+1,n):
+		share[i][j] = intervm_sharing(file_name[i], file_name[j]);
+
+bins_no = 3;
+binsize = [0 for x in range(bins_no)];
+
+binsize[0] = 3010;
+binsize[1] = 3010;
+binsize[2] = 3010;
+
+maxi = 0;
+max_posx = 0;
+max_posy = 0;
+
+matched = [-1 for x in range(n)];
+
+for i in range(n):
+	for j in range(n):
+		if (maxi < share[i][j]):
+			maxi = share[i][j];
+			max_posx = i;
+			max_posy = j;
+
+
+binsize[0] = binsize[0] - vm_size[max_posx];
+binsize[0] = binsize[0] - vm_size[max_posy];
+matched[max_posx] = 0;
+matched[max_posy] = 0;
+#print(matched);
+best = 0;
+best_pos = -1;
+curr_bin = 0;
+count = 0;
+
+i = 0;
+while i < n-2:
+	best = 0;
+	count = 0;
+	best_pos = -1;
+	for j in range(n):
+		temp = 0;
+		count = 0;
+		if (matched[j] != -1):
+			continue;
+		for k in range(n):
+			if (matched[k] == curr_bin):
+				temp = temp + share[j][k]/(count+1.0);
+				count = count + 1;
+		if (temp >= best):
+			best = temp;
+			best_pos = j;
+
+	if (binsize[curr_bin] > vm_size[best_pos]):
+		print(best_pos);
+		matched[best_pos] = curr_bin
+	else:
+		if (curr_bin == bins_no):
+			print("No more available machines\n");
+		else:
+			curr_bin = curr_bin + 1;
+			i = i - 1;
+
+			maxi = 0;
+			max_posx = -1;
+			max_posy = -1;
+
+			for k in range(n):
+				if (matched[k] != -1):
+					continue;
+				for l in range(n):
+					if (k == l):
+						continue;
+					if (matched[l] != -1):
+						continue;
+					if (maxi < share[k][l]):
+						maxi = share[k][l];
+						max_posx = k;
+						max_posy = l;
+
+			if (max_posy == -1):
+				for k in range(n):
+					if (matched[k] == -1):
+						matched[k] = curr_bin;
+				break;
+
+			binsize[curr_bin] = binsize[curr_bin] - vm_size[max_posx];
+			binsize[curr_bin] = binsize[curr_bin] - vm_size[max_posy];
+			matched[max_posx] = curr_bin;
+			matched[max_posy] = curr_bin;
+			i = i + 1;
+	i = i+1;
+
+for i in range(n):
+	print ('VM #',i,' goes to machine', matched[i])
+
+initial_mem = 0;
+final_mem = 0;
+
+for i in range(bins_no):
+	str_set = [];
+	for j in range(n):
+		if (initial_map[j] == i):
+			str_set = str_set + [file_name[j]];
+	temp = final_usage(str_set)
+	initial_mem = initial_mem + temp;
+	print('Memory usage before colocation on physical machine #',i," is ",temp,"MB")
+
+for i in range(bins_no):
+	str_set = [];
+	for j in range(n):
+		if (matched[j] == i):
+			str_set = str_set + [file_name[j]];
+	temp = final_usage(str_set)
+	final_mem = final_mem + temp;
+	print('Memory usage on physical machine #',i," is ",temp,"MB")
+
+print('Initial usage was',initial_mem,"MB");
+print('After colocation memory usage was',final_mem,"MB");
+print('Memory saved is ',initial_mem - final_mem ,"MB")
